8e480ef415a01875df6faf5494535bf69c1d75cb
Em Poertner <e.poertner@yahoo.com>
2019-11-19 17:13:38 -0500

    Make check primality a function

--- PracticingPython.org/checkprimality.py
+++ PracticingPython.org/checkprimality.py
@@ -2,12 +2,16 @@
 #A prime number is a number that has no divisors.
 userNumber = input("Type a number to see if it's prime. ")
 userNumInt = int(userNumber)
-if userNumInt > 1:
-    for i in range(2, userNumInt//2):
-        if (userNumInt % i) == 0:
-            print(userNumber + " is not a prime number.")
-            break
+def checkPrimality(n):
+    if userNumInt > 1:
+        for i in range(2, userNumInt//2):
+            if (userNumInt % i) == 0:
+                return False
+        else:
+            return True
     else:
-        print(userNumber + " is a prime number.")
+        return False
+if (checkPrimality(userNumInt)) == True:
+    print(userNumber + " is a prime number.")
 else:
-    print(userNumber + " is not a prime number")
+    print(userNumber + " is not a prime number.")
