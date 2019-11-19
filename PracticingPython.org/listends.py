9fbb04d39631ca3039fb47e111b032efc42f1813
Em Poertner <e.poertner@yahoo.com>
2019-11-19 17:35:48 -0500

    Add file for List End challenge

---
+++ PracticingPython.org/listends.py
@@ -0,0 +1,3 @@
+#Write a program that takes a list of numbers and makes a new list of only the first and last elements of the given list.
+#For practice, write this code inside a function.
+a = [5, 10, 15, 20, 25]
