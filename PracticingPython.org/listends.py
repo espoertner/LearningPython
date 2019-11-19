9be1e019832ee7ac218cae2bda9da1969b7a9dbf
Em Poertner <e.poertner@yahoo.com>
2019-11-19 17:57:11 -0500

    Add solution to list end challenge

--- PracticingPython.org/listends.py
+++ PracticingPython.org/listends.py
@@ -1,3 +1,8 @@
 #Write a program that takes a list of numbers and makes a new list of only the first and last elements of the given list.
 #For practice, write this code inside a function.
 a = [5, 10, 15, 20, 25]
+def firstLastItem(OriginalList):
+    length = len(OriginalList)
+    newList = [OriginalList[0], OriginalList[length-1]]
+    print (newList)
+firstLastItem(a)
